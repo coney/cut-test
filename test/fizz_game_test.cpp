98c707c39fe03fee0cf1cdd2b0e7dff8a3ff926c
Coney Wu <coney.wu@gmail.com>
2016-08-11 00:38:58 +0800

    di

--- test/fizz_game_test.cpp
+++ test/fizz_game_test.cpp
@@ -3,33 +3,45 @@
 extern "C" {
     #include "fizz_game.h"
 }
-DECLARE_FUNCTION_MOCK2(DefaultRuleMock, default_rule, const char *(int, int *));
-DECLARE_FUNCTION_MOCK2(MultipleRuleMock, multiple_rule, const char *(int, int *));
-DECLARE_FUNCTION_MOCK2(SpecialWordRuleMock, special_word_rule, const char *(int, int *));
-
-IMPLEMENT_FUNCTION_MOCK2(DefaultRuleMock, default_rule, const char *(int, int *));
-IMPLEMENT_FUNCTION_MOCK2(MultipleRuleMock, multiple_rule, const char *(int, int *));
-IMPLEMENT_FUNCTION_MOCK2(SpecialWordRuleMock, special_word_rule, const char *(int, int *));
 
 using namespace ::testing;
+
+static int numbers[] = { 3,5,7 };
+
+TEST(default_rule, ShouldReturnNumberDirectly) {
+    ASSERT_STREQ("1", default_rule(1, numbers));
+    ASSERT_STREQ("2", default_rule(2, numbers));
+    ASSERT_STREQ("13", default_rule(13, numbers));
+
+}
+
+TEST(multiple_rule, ShouldReturnWordsAccordingToMultiple) {
+    ASSERT_STREQ("Fizz", multiple_rule(3, numbers));
+    ASSERT_STREQ("BuzzWhizz", multiple_rule(35, numbers));
+    ASSERT_STREQ("FizzBuzzWhizz", multiple_rule(105, numbers));
+    ASSERT_EQ(NULL, special_word_rule(2, numbers));
+
+}
+
+TEST(special_word_rule, ShouldReturnWordAccordingToSpecialNumber) {
+    ASSERT_STREQ("Fizz", special_word_rule(3, numbers));
+    ASSERT_STREQ("Fizz", special_word_rule(13, numbers));
+    ASSERT_STREQ("Fizz", special_word_rule(35, numbers));
+    ASSERT_EQ(NULL, special_word_rule(9, numbers));
+}
+
+
+
 TEST(Student, ShouldSayWordAccordingToRules) {
     rule_fn rules[] = {
         special_word_rule, multiple_rule, default_rule
     };
 
-    int numbers[] = { 3, 5, 7 };
-    SpecialWordRuleMock specialWordRuleMock;
-    EXPECT_FUNCTION_CALL(specialWordRuleMock, (_, _)).WillRepeatedly(Return((const char *)NULL));
-    EXPECT_FUNCTION_CALL(specialWordRuleMock, (3, _)).WillRepeatedly(Return("Hello"));
-
-    MultipleRuleMock multipleRuleMock;
-    EXPECT_FUNCTION_CALL(multipleRuleMock, (_, _)).WillRepeatedly(Return((const char *)NULL));
-    EXPECT_FUNCTION_CALL(multipleRuleMock, (5, _)).WillRepeatedly(Return("World"));
-
-    DefaultRuleMock defaultRuleMock;
-    EXPECT_FUNCTION_CALL(defaultRuleMock, (_, _)).WillRepeatedly(Return("Nothing"));
-
-    ASSERT_STREQ("Hello", student_say(3, numbers, rules, 3));
-    ASSERT_STREQ("World", student_say(5, numbers, rules, 3));
-    ASSERT_STREQ("Nothing", student_say(7, numbers, rules, 3));
+    ASSERT_STREQ("1", student_say(1, numbers, rules, 3));
+    ASSERT_STREQ("Fizz", student_say(3, numbers, rules, 3));
+    ASSERT_STREQ("Buzz", student_say(5, numbers, rules, 3));
+    ASSERT_STREQ("Whizz", student_say(7, numbers, rules, 3));
+    ASSERT_STREQ("FizzBuzz", student_say(15, numbers, rules, 3));
+    ASSERT_STREQ("FizzBuzzWhizz", student_say(105, numbers, rules, 3));
+    ASSERT_STREQ("Fizz", student_say(35, numbers, rules, 3));
 }
